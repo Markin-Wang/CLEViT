412ddb4fe170915faf822178d5f7dbe8faf5da2d
Markin-Wang <cserwj@gmail.com>
2022-11-21 23:39:44 +0000

    replace scipy imread with imageio.imread

--- data/dataset.py
+++ data/dataset.py
@@ -67,12 +67,12 @@ class CUB():
         train_file_list = [x for i, x in zip(train_test_list, img_name_list) if i]
         test_file_list = [x for i, x in zip(train_test_list, img_name_list) if not i]
         if self.is_train:
-            self.train_img = [scipy.misc.imread(os.path.join(self.root, 'images', train_file)) for train_file in
+            self.train_img = [imageio.imread(os.path.join(self.root, 'images', train_file)) for train_file in
                               train_file_list[:data_len]]
             self.train_label = [x for i, x in zip(train_test_list, label_list) if i][:data_len]
             self.train_imgname = [x for x in train_file_list[:data_len]]
         if not self.is_train:
-            self.test_img = [scipy.misc.imread(os.path.join(self.root, 'images', test_file)) for test_file in
+            self.test_img = [imageio.imread(os.path.join(self.root, 'images', test_file)) for test_file in
                              test_file_list[:data_len]]
             self.test_label = [x for i, x in zip(train_test_list, label_list) if not i][:data_len]
             self.test_imgname = [x for x in test_file_list[:data_len]]
@@ -698,7 +698,7 @@ class soybean_gene():
         
     def __getitem__(self, index): 
         img_path = os.path.join(self.root, 'images', self.imgs_name[index])
-        img, target, imgname = scipy.misc.imread(img_path), self.labels[index], self.imgs_name[index]
+        img, target, imgname = imageio.imread(img_path), self.labels[index], self.imgs_name[index]
         if len(img.shape) == 2:
             img = np.stack([img] * 3, 2)
         img = Image.fromarray(img, mode='RGB')
@@ -726,7 +726,7 @@ class AFD():
         
     def __getitem__(self, index): 
         img_path = os.path.join(self.root, 'images', self.imgs_name[index])
-        img, target, imgname = scipy.misc.imread(img_path), self.labels[index], self.imgs_name[index]
+        img, target, imgname = imageio.imread(img_path), self.labels[index], self.imgs_name[index]
         if len(img.shape) == 2:
             img = np.stack([img] * 3, 2)
         img = Image.fromarray(img, mode='RGB')
@@ -754,7 +754,7 @@ class WRD():
         
     def __getitem__(self, index): 
         img_path = os.path.join(self.root, 'images', self.imgs_name[index])
-        img, target, imgname = scipy.misc.imread(img_path), self.labels[index], self.imgs_name[index]
+        img, target, imgname = imageio.imread(img_path), self.labels[index], self.imgs_name[index]
         if len(img.shape) == 2:
             img = np.stack([img] * 3, 2)
         img = Image.fromarray(img, mode='RGB')
@@ -786,7 +786,7 @@ class FGVC_aircraft():
 
     def __getitem__(self, index):
         if self.is_train:
-            img, target = scipy.misc.imread(self.train_img_label[index][0]), self.train_img_label[index][1]
+            img, target = imageio.imread(self.train_img_label[index][0]), self.train_img_label[index][1]
             if len(img.shape) == 2:
                 img = np.stack([img] * 3, 2)
             img = Image.fromarray(img, mode='RGB')
@@ -794,7 +794,7 @@ class FGVC_aircraft():
                 img = self.transform(img)
 
         else:
-            img, target = scipy.misc.imread(self.test_img_label[index][0]), self.test_img_label[index][1]
+            img, target = imageio.imread(self.test_img_label[index][0]), self.test_img_label[index][1]
             if len(img.shape) == 2:
                 img = np.stack([img] * 3, 2)
             img = Image.fromarray(img, mode='RGB')
@@ -879,7 +879,7 @@ class soybean_aging_R3():
         
     def __getitem__(self, index): 
         img_path = os.path.join(self.root, 'images', self.imgs_name[index])
-        img, target, imgname = scipy.misc.imread(img_path), self.labels[index], self.imgs_name[index]
+        img, target, imgname = imageio.imread(img_path), self.labels[index], self.imgs_name[index]
         if len(img.shape) == 2:
             img = np.stack([img] * 3, 2)
         img = Image.fromarray(img, mode='RGB')
