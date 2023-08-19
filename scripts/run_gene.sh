10aa1d269d2a4464045cfb65c4f59d24e8124045
Jun Wang <45941861+Markin-Wang@users.noreply.github.com>
2023-08-19 01:06:57 +0100

    Update run_gene.sh

--- scripts/run_gene.sh
+++ scripts/run_gene.sh
@@ -3,7 +3,7 @@ python -m torch.distributed.launch --nproc_per_node 1 --master_port 12345  main.
 --dataset soybean_gene \
 --tag $tag \
 --lr 2e-3 \
---model full_b \
+--model full \
 --mask \
 --swap \
 --con \
